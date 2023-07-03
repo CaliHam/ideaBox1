d527f6638202afe1bad61221c4753a936c84f162
Calli Herrmann <calliherrmann@gmail.com>
2023-07-03 15:57:51 -0400

    Add sample ideas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
 import { useState } from 'react';
 
 const App = () => {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+]
+  const [ideas, setIdeas] = useState(dummyIdeas);
+
   return (
-    <h1>IdeaBox</h1>
+    <main className='App'>
+      <h1>IdeaBox</h1>
+      <p>Hi!</p>
+    </main>
   );
 }
 
