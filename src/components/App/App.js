c46225665487643c8a8d8b529ffece94aa53895c
Calli Herrmann <calliherrmann@gmail.com>
2023-07-03 16:57:51 -0400

    Add functioning form element

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,18 +3,23 @@ import Ideas from '../Ideas/Ideas';
 import Form from '../Form/Form';
 import { useState } from 'react';
 
+
 const App = () => {
   const dummyIdeas = [
     { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
     { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
     { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
-]
+  ]
   const [ideas, setIdeas] = useState(dummyIdeas);
+  
+  const addIdea = (newIdea) => {
+    setIdeas([...ideas, newIdea])
+  }
 
   return (
     <main className='App'>
       <h1>IdeaBox</h1>
-      <Form />
+      <Form addIdea={addIdea}/>
       {!ideas.length && <h2>No ideas yet -- add some!</h2>}
       <Ideas ideas={ideas} />
     </main>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import './Form.css';
 
-const Form = () => {
+const Form = ({addIdea}) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
+    const submitIdeas = (e) => {
+        e.preventDefault()
+        const newIdea = {
+            id: Date.now(),
+            title,
+            description
+        }
+        addIdea(newIdea)
+        clearInput()
+    }
+
+    const clearInput = () => {
+        setTitle('')
+        setDescription('')
+    }
+
     return (
         <form>
-            <input type='text' placeholder='Title' name='title' value={title} />
-            <input type='text' placeholder='Desciption' name='description' value={description}/>
-            <button>SUBMIT</button>
+            <input type='text' placeholder='Title' name='title' value={title} onChange={event => setTitle(event.target.value)}/>
+            <input type='text' placeholder='Desciption' name='description' value={description} onChange={event => setDescription(event.target.value)}/>
+            <button onClick={ e => submitIdeas(e)}>SUBMIT</button>
         </form>
     )
 }
