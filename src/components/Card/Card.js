83847950b25366dace66772008960f4b2b9de7f3
Calli Herrmann <calliherrmann@gmail.com>
2023-07-08 17:08:59 -0400

    Add trashcan image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
 import './Card.css';
 import Trash from '../../assets/delete.png'
 
-const Card = ({ title, description, id, deleteIdea}) => {
+const Card = ({ title, description, id, deleteIdea }) => {
     return (
         <div className='card'>
             <h3>{title}</h3>
             <p>{description}</p>
-            <button onClick={() => deleteIdea(id)}><img src={Trash}/></button>
+            <button onClick={() => deleteIdea(id)}><img src={Trash} alt='trash'/></button>
         </div>
     )
 }
